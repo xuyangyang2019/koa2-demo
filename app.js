e58e83740f4e624abbefe38f6331877399dfef4d
xuyangyang2019 <15268296911@163.com>
2019-11-20 16:58:12 +0800

    处理url

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
+// 注意require('koa-router')返回的是函数:
+const router = require('koa-router')();
+
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
+    // 用户鉴权
+    // if (await checkUserPermission(ctx)) {
+    //     await next(); // 调用下一个middleware
+    // } else {
+    //     ctx.response.status = 403;
+    // }
 });
+
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+
+// add router middleware:
+app.use(router.routes());
+
+// app.use(async (ctx, next) => {
+//     const start = new Date().getTime(); // 当前时间
+//     await next(); // 调用下一个middleware
+//     const ms = new Date().getTime() - start; // 耗费时间
+//     console.log(`Time: ${ms}ms`); // 打印耗费时间
+// });
+
+// app.use(async (ctx, next) => {
+//     await next();
+//     ctx.response.type = 'text/html';
+//     ctx.response.body = '<h1>Hello, koa2!</h1>';
+// });
 
 // 在端口3000监听:
 app.listen(3000);
