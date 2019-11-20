0dd03e8b672c2987797649ad3a0fe2eff15198c1
xuyangyang2019 <15268296911@163.com>
2019-11-20 17:16:19 +0800

    处理post请求

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const Koa = require('koa');
 // 注意require('koa-router')返回的是函数:
 const router = require('koa-router')();
 
+// 解析原始request请求
+const bodyParser = require('koa-bodyparser');
+
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
+
+// app.use(bodyParser());
 
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
@@ -19,15 +24,56 @@ app.use(async (ctx, next) => {
     // }
 });
 
+// app.use(async (ctx, next) => {
+//     if (ctx.request.path === '/') {
+//         ctx.response.body = 'index page';
+//     } else {
+//         await next();
+//     }
+// });
+
+// app.use(async (ctx, next) => {
+//     if (ctx.request.path === '/test') {
+//         ctx.response.body = 'TEST page';
+//     } else {
+//         await next();
+//     }
+// });
+
+
 // add url-route:
 router.get('/hello/:name', async (ctx, next) => {
     var name = ctx.params.name;
     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
 
+// router.get('/', async (ctx, next) => {
+//     ctx.response.body = '<h1>Index</h1>';
+// });
+
 router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>';
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
 });
+
+router.post('/signin', async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+
+app.use(bodyParser());
 
 // add router middleware:
 app.use(router.routes());
