5ba1115e0c9f6f96a98b65519620f736c8910f2a
xuyangyang2019 <15268296911@163.com>
2019-11-20 19:01:50 +0800

    nunjucks模板引擎示例

--- app.js
+++ app.js
@@ -4,8 +4,56 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 // 导入controller middleware:
 const controller = require('./controller');
-// // 注意require('koa-router')返回的是函数:
-// const router = require('koa-router')();
+
+// 模板引擎
+const nunjucks = require('nunjucks');
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+            autoescape: autoescape,
+            throwOnUndefined: throwOnUndefined
+        });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
+
+var s = env.render('hello.html', { name: '小明' });
+// 模板可以防止xss攻击
+// var s = env.render('hello.html', { name: '<script>alert("小明")</script>' });
+// http://mozilla.github.io/nunjucks/getting-started.html
+// console.log(s);
+
+// var s = env.render('hello.html', {
+//     name: '<Nunjucks>',
+//     fruits: ['Apple', 'Pear', 'Banana'],
+//     count: 12000
+// });
+// console.log(s);
+
+console.log(env.render('extend.html', {
+    header: 'Hello',
+    body: 'bla bla bla...'
+}));
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
