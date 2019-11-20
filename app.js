8daa207acce6ccd87781ef1e9c4fd58aadcb7688
xuyangyang2019 <15268296911@163.com>
2019-11-20 17:40:58 +0800

    router拆分 自动导入

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
-
 // 注意require('koa-router')返回的是函数:
 const router = require('koa-router')();
-
 // 解析原始request请求
 const bodyParser = require('koa-bodyparser');
+
+const fs = require('fs');
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 
-// app.use(bodyParser());
+// 先导入fs模块，然后用readdirSync列出文件
+// 这里可以用sync是因为启动时只运行一次，不存在性能问题:
+let files = fs.readdirSync(__dirname + '/controllers');
+// 过滤出.js文件:
+let js_files = files.filter((f) => {
+    return f.endsWith('.js');
+});
+// 处理每个js文件:
+for (const f of js_files) {
+    console.log(`js文件: ${f}...`);
+    // 导入js文件:
+    let mapping = require(__dirname + '/controllers/' + f);
+    for (const url in mapping) {
+        if (url.startsWith('GET ')) {
+            // 如果url类似"GET xxx":
+            let path = url.substring(4);
+            router.get(path, mapping[url]);
+        } else if (url.startsWith('POST ')) {
+            // 如果url类似"POST xxx":
+            let path = url.substring(5);
+            router.post(path, mapping[url]);
+        } else {
+            // 无效的URL:
+            console.log(`invalid URL: ${url}`);
+        }
+    }
+}
 
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
+    console.log('打印URL')
     console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
     await next(); // 调用下一个middleware
     // 用户鉴权
@@ -23,73 +50,10 @@ app.use(async (ctx, next) => {
     //     ctx.response.status = 403;
     // }
 });
-
-// app.use(async (ctx, next) => {
-//     if (ctx.request.path === '/') {
-//         ctx.response.body = 'index page';
-//     } else {
-//         await next();
-//     }
-// });
-
-// app.use(async (ctx, next) => {
-//     if (ctx.request.path === '/test') {
-//         ctx.response.body = 'TEST page';
-//     } else {
-//         await next();
-//     }
-// });
-
-
-// add url-route:
-router.get('/hello/:name', async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
-
-// router.get('/', async (ctx, next) => {
-//     ctx.response.body = '<h1>Index</h1>';
-// });
-
-router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
-        <form action="/signin" method="post">
-            <p>Name: <input name="name" value="koa"></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="Submit"></p>
-        </form>`;
-});
-
-router.post('/signin', async (ctx, next) => {
-    var
-        name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
-    }
-});
-
+// 由于middleware的顺序很重要，这个koa-bodyparser必须在router之前被注册到app对象上。
 app.use(bodyParser());
-
 // add router middleware:
 app.use(router.routes());
-
-// app.use(async (ctx, next) => {
-//     const start = new Date().getTime(); // 当前时间
-//     await next(); // 调用下一个middleware
-//     const ms = new Date().getTime() - start; // 耗费时间
-//     console.log(`Time: ${ms}ms`); // 打印耗费时间
-// });
-
-// app.use(async (ctx, next) => {
-//     await next();
-//     ctx.response.type = 'text/html';
-//     ctx.response.body = '<h1>Hello, koa2!</h1>';
-// });
 
 // 在端口3000监听:
 app.listen(3000);
