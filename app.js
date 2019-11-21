74c06fa24709b455a92bf110fd3b520eb55b77ba
xuyangyang2019 <15268296911@163.com>
2019-11-21 16:43:30 +0800

    添加mysql 驱动和orm

--- app.js
+++ app.js
@@ -7,6 +7,86 @@ const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
 const templating = require('./templating');
 
+// mysql
+const Sequelize = require('sequelize');
+const config = require('./mysql/config');
+
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
+});
+
+var Pet = sequelize.define('pet', {
+    id: {
+        type: Sequelize.STRING(50),
+        primaryKey: true
+    },
+    name: Sequelize.STRING(100),
+    gender: Sequelize.BOOLEAN,
+    birth: Sequelize.STRING(10),
+    createdAt: Sequelize.BIGINT,
+    updatedAt: Sequelize.BIGINT,
+    version: Sequelize.BIGINT
+}, {
+    timestamps: false
+});
+
+var now = Date.now();
+
+Pet.create({
+    id: 'g-' + now,
+    name: 'Gaffey',
+    gender: false,
+    birth: '2007-07-07',
+    createdAt: now,
+    updatedAt: now,
+    version: 0
+}).then(function (p) {
+    console.log('created.' + JSON.stringify(p));
+}).catch(function (err) {
+    console.log('failed: ' + err);
+});
+
+(async () => {
+    var dog = await Pet.create({
+        id: 'd-' + now,
+        name: 'Odie',
+        gender: false,
+        birth: '2008-08-08',
+        createdAt: now,
+        updatedAt: now,
+        version: 0
+    });
+    console.log('created: ' + JSON.stringify(dog));
+})();
+
+(async () => {
+    var pets = await Pet.findAll({
+        where: {
+            name: 'Gaffey'
+        }
+    });
+    console.log(`find ${pets.length} pets:`);
+    for (let p of pets) {
+        console.log(JSON.stringify(p));
+        console.log('update pet...');
+        p.gender = true;
+        p.updatedAt = Date.now();
+        p.version++;
+        await p.save();
+        if (p.version === 3) {
+            await p.destroy();
+            console.log(`${p.name} was destroyed.`);
+        }
+    }
+})();
+
+
 // 判断当前环境是否是production环境 production development
 const isProduction = process.env.NODE_ENV === 'production';
 // 创建一个Koa对象表示web app本身:
@@ -22,49 +102,21 @@ app.use(async (ctx, next) => {
     execTime = new Date().getTime() - start;
     ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
-// 对于任何请求，app将调用该异步函数处理请求：
-// app.use(async (ctx, next) => {
-//     console.log('打印URL')
-//     console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
-//     await next(); // 调用下一个middleware
-//     // 用户鉴权
-//     // if (await checkUserPermission(ctx)) {
-//     //     await next(); // 调用下一个middleware
-//     // } else {
-//     //     ctx.response.status = 403;
-//     // }
-// });
 
-// 第二个middleware处理静态文件：
-// 生产环境下，静态文件是由部署在最前面的反向代理服务器（如Nginx）处理的，Node程序不需要处理静态文件。
-// 而在开发环境下，我们希望koa能顺带处理静态文件，
 if (!isProduction) {
     let staticFiles = require('./static-files');
     app.use(staticFiles('/static/', __dirname + '/static'));
 }
 
-// 第三个middleware解析POST请求：
-// 由于middleware的顺序很重要，这个koa-bodyparser必须在router之前被注册到app对象上。
 app.use(bodyParser());
 
-// 第四个middleware负责给ctx加上render()来使用Nunjucks：
-// 如果是开发环境，就使用缓存，如果不是，就关闭缓存
-// 在开发环境下，关闭缓存后，我们修改View，可以直接刷新浏览器看到效果，
-// 否则，每次修改都必须重启Node程序，会极大地降低开发效率。
-// app.use(templating('views', {
-//     noCache: !isProduction,
-//     watch: !isProduction
-// }));
 templating('views', {
     noCache: !isProduction,
     watch: !isProduction
 }, app);
 
-// 最后一个middleware处理URL路由：
-// 使用middleware:创建、绑定router
 app.use(controller());
 
 // 在端口3000监听:
 app.listen(3000);
 console.log('app started at port 3000...');
-console.log(process.env.NODE_ENV);
