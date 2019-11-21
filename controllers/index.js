cdc8503a28e830c4f87e3ae6ef494e9c8b17060c
xuyangyang2019 <15268296911@163.com>
2019-11-21 11:45:31 +0800

    首页controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 var fn_index = async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
-        <form action="/signin" method="post">
-            <p>Name: <input name="name" value="koa"></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="Submit"></p>
-        </form>`;
+    // ctx.response.body = `<h1>Index</h1>
+    //     <form action="/signin" method="post">
+    //         <p>Name: <input name="name" value="koa"></p>
+    //         <p>Password: <input name="password" type="password"></p>
+    //         <p><input type="submit" value="Submit"></p>
+    //     </form>`;
+    ctx.render('index.html', {
+        title: 'Welcome'
+    });
 };
 
 var fn_signin = async (ctx, next) => {
