817d51be678e7fb1266ea3c8f64c3b9a28450f98
xuyangyang2019 <15268296911@163.com>
2019-11-21 16:41:08 +0800

    mysql配置文件

---
+++ mysql/config.js
@@ -0,0 +1,9 @@
+var config = {
+    database: 'test', // 使用哪个数据库
+    username: 'test', // 用户名
+    password: 'test', // 口令
+    host: 'localhost', // 主机名
+    port: 3306 // 端口号，MySQL默认3306
+};
+
+module.exports = config;
