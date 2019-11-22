740e74207b062c374f04eb683ec19c49a4687855
xuyangyang2019 <15268296911@163.com>
2019-11-22 09:46:18 +0800

    mysql 测试环境的配置文件

---
+++ mysql/config-test.js
@@ -0,0 +1,11 @@
+// 存储用于测试的配置
+var config = {
+    dialect: 'mysql',
+    database: 'test',
+    username: 'test',
+    password: 'test',
+    host: 'localhost',
+    port: 3306
+};
+
+module.exports = config;
