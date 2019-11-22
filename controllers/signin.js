a34667cfde66b33127ece1d99c81dd15bc471a20
xuyangyang2019 <15268296911@163.com>
2019-11-22 19:32:30 +0800

    从数据库查询用户数据 确定是否登陆

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,15 +1,28 @@
-var fn_signin = async (ctx, next) => {
-    var
+const model = require('../model');
+let User = model.User;
+
+let fn_signin = async (ctx, next) => {
+    let
         email = ctx.request.body.email || '',
         password = ctx.request.body.password || '';
-    if (email === 'admin@example.com' && password === '123456') {
-        console.log('signin ok!');
+
+    var users = await User.findAll({
+        where: {
+            email: email,
+            passwd: password
+        }
+    });
+    // user应该保证唯一性
+    // console.log(`find ${users.length} users:`);
+    if (users.length > 0) {
+        // console.log('signin ok!');
+        let currentUser = users[0]
         ctx.render('signin-ok.html', {
             title: 'Sign In OK',
-            name: 'Mr Node'
+            name: currentUser.name
         });
     } else {
-        console.log('signin failed!');
+        // console.log('signin failed!');
         ctx.render('signin-failed.html', {
             title: 'Sign In Failed'
         });
