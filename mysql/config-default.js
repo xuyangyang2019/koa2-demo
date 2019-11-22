b8c4ae5e3094c7aa6a064531facfa84f18ef97e8
xuyangyang2019 <15268296911@163.com>
2019-11-22 09:45:56 +0800

    mysql 默认配置文件

---
+++ mysql/config-default.js
@@ -0,0 +1,11 @@
+// 存储默认的配置
+var config = {
+    dialect: 'mysql',
+    database: 'test',
+    username: 'test',
+    password: 'test',
+    host: 'localhost',
+    port: 3306
+};
+
+module.exports = config;
