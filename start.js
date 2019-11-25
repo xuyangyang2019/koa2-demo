68456185c05cd9ac8a51a96a7369c9aa3ec2da09
xuyangyang2019 <15268296911@163.com>
2019-11-25 11:05:21 +0800

    启动文件 启用websock和测试

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 // start.js
 // 负责真正启动应用
 const app = require('./app');
+require('./websocket/websocketServer')
+require('./websocket/websocketClient')
 
 app.listen(3000);
 console.log('app started at port 3000...');
