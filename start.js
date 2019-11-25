7cd727325925daa23f721182debca7f76f201d32
xuyangyang2019 <15268296911@163.com>
2019-11-25 16:47:20 +0800

    不导入websocket

--- start.js
+++ start.js
@@ -1,8 +1,6 @@
 // start.js
 // 负责真正启动应用
 const app = require('./app');
-require('./websocket/websocketServer')
-require('./websocket/websocketClient')
 
 app.listen(3000);
 console.log('app started at port 3000...');
