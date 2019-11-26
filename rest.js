a776c7eb793958962e56f926b70c0e684800c05b
xuyangyang2019 <15268296911@163.com>
2019-11-26 10:58:16 +0800

    添加错误处理接口

--- rest.js
+++ rest.js
@@ -1,4 +1,8 @@
 module.exports = {
+    APIError: function (code, message) {
+        this.code = code || 'internal:unknown_error';
+        this.message = message || '';
+    },
     restify: (pathPrefix) => {
         // REST API前缀，默认为/api/:
         pathPrefix = pathPrefix || '/api/';
@@ -10,7 +14,17 @@ module.exports = {
                     ctx.response.type = 'application/json';
                     ctx.response.body = data;
                 }
-                await next();
+                try {
+                    await next();
+                } catch (e) {
+                    console.log('Process API error...');
+                    ctx.response.status = 400;
+                    ctx.response.type = 'application/json';
+                    ctx.response.body = {
+                        code: e.code || 'internal:unknown_error',
+                        message: e.message || ''
+                    };
+                }
             } else {
                 await next();
             }
