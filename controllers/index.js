8c41347fad7ea0d30fc1cce5ddf96877684ec562
xuyangyang2019 <15268296911@163.com>
2019-11-26 16:45:23 +0800

    将备份中的代码 整合到index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,26 @@
 // MVC Controllers
 
+// let fn_hello = async (ctx, next) => {
+//     ctx.response.type = 'text/html';
+//     let name = ctx.request.query.name || 'world';
+//     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+// };
+
+// module.exports = {
+//     'GET /hello/:name': fn_hello
+// };
+
+
+// let fn_index = async (ctx, next) => {
+//     ctx.render('index.html', {
+//         title: 'Welcome'
+//     });
+// };
+
+// module.exports = {
+//     'GET /': fn_index
+// };
+
 module.exports = {
     'GET /': async (ctx, next) => {
         // websocket demo
@@ -11,6 +32,7 @@ module.exports = {
         // } else {
         //     ctx.response.redirect('/signin');
         // }
+
         // rest demo
         ctx.render('index.html');
     }
