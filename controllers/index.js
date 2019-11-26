bce910cc515b37844ce5cede6f2276bec1c8763c
xuyangyang2019 <15268296911@163.com>
2019-11-26 11:01:04 +0800

    首页改为演示rest

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,17 @@
-// index:
+// MVC Controllers
 
 module.exports = {
     'GET /': async (ctx, next) => {
-        let user = ctx.state.user;
-        if (user) {
-            ctx.render('roomVue.html', {
-                user: user
-            });
-        } else {
-            ctx.response.redirect('/signin');
-        }
+        // websocket demo
+        // let user = ctx.state.user;
+        // if (user) {
+        //     ctx.render('roomVue.html', {
+        //         user: user
+        //     });
+        // } else {
+        //     ctx.response.redirect('/signin');
+        // }
+        // rest demo
+        ctx.render('index.html');
     }
 };
