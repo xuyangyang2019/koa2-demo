fd7c9fd2cdeae381ff5752bdf403dade6c24dc4e
xuyangyang2019 <15268296911@163.com>
2019-11-27 14:50:26 +0800

    配置文件

---
+++ config/config.prod.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // 数据库配置
+    DB_DIALECT: 'mysql',
+    DB_HOST: 'localhost',
+    DB_PORT: 3306,
+    DB_NAME: 'test',
+    DB_USER: 'test',
+    DB_PASS: 'test',
+
+    // // HTTP端口
+    // HTTP_PORT: 8080,
+    // // 静态文件绝对路径
+    // HTTP_ROOT: path.resolve(__dirname, '../static/'),
+    // // 上传文件保存绝对路径
+    // HTTP_UPLOAD: path.resolve(__dirname, '../static/upload')
+}
---
+++ config/index.js
@@ -0,0 +1,14 @@
+const process = require('process')
+
+// 可以通过开发环境和生产环境系统等参数差异，判断处于哪个环境。
+// const mode = process.env.OS === 'Windows_NT' ? 'env' : 'prod'
+
+// 也可以通过package.json中配置的启动命令判断处于开发还是生产环境。
+const mode = process.argv[2] === '--prod' ? 'prod' : 'dev'
+
+console.log(mode)
+
+module.exports = {
+    mode, // 当前所处环境
+    ...(mode === 'production' ? require('./config.prod') : require('./config.dev'))  // 当前环境的配置
+}
