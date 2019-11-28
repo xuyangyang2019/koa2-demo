90c1e7b2fd0cac9273777eac89f31be1099f05f6
xuyangyang2019 <15268296911@163.com>
2019-11-28 14:24:48 +0800

    数据库调试

--- controllers/index.js
+++ controllers/index.js
@@ -25,17 +25,17 @@
 module.exports = {
     'GET /': async (ctx, next) => {
         // websocket demo
-        let user = ctx.state.user;
-        if (user) {
-            ctx.render('roomVue.html', {
-                user: user
-            });
-        } else {
-            ctx.response.redirect('/signin');
-        }
+        // let user = ctx.state.user;
+        // if (user) {
+        //     ctx.render('roomVue.html', {
+        //         user: user
+        //     });
+        // } else {
+        //     ctx.response.redirect('/signin');
+        // }
 
         // mysql demo
-        // ctx.render('index.html');
+        ctx.render('index.html');
 
         // rest demo
         // ctx.render('indexVue.html');
