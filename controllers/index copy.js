bbb54b27ee5b689f60ee075fa0a9a14dd7127bd8
xuyangyang2019 <15268296911@163.com>
2020-08-31 00:02:09 +0800

    备份

---
+++ controllers/index copy.js
@@ -0,0 +1,50 @@
+// MVC Controllers
+
+// 原始语法
+// let fn_hello = async (ctx, next) => {
+//     ctx.response.type = 'text/html';
+//     let name = ctx.request.query.name || 'world';
+//     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+// };
+
+// module.exports = {
+//     'GET /hello/:name': fn_hello
+// };
+
+// Nunjucks 模板方法
+// let fn_index = async (ctx, next) => {
+//     ctx.render('index.html', {N
+//         title: 'Welcome'
+//     });
+// };
+
+// module.exports = {
+//     'GET /': fn_index
+// };
+
+module.exports = {
+    'GET /': async (ctx, next) => {
+        // websocket demo
+        // let user = ctx.state.user;
+        // if (user) {
+        //     ctx.render('roomVue.html', {
+        //         user: user
+        //     });
+        // } else {
+        //     ctx.response.redirect('/signin');
+        // }
+
+        // mysql demo
+        // ctx.render('index.html');
+        ctx.render('demo-unjucks.html', {
+            title: 'unjucks-upload-page',
+            header: 'Hello',
+            main: 'bla bla bla...'
+        })
+        // let html = await render('demo.html')
+        // ctx.body = html
+
+        // rest demo
+        // ctx.render('indexVue.html');
+    }
+};
