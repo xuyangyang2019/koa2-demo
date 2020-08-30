5c64d23400cc583e583e5420199c75ac036c811c
xuyangyang2019 <15268296911@163.com>
2020-08-30 23:59:44 +0800

    操作层目录

--- controllers/index.js
+++ controllers/index.js
@@ -1,43 +1,6 @@
-// MVC Controllers
-
-// 原始语法
-// let fn_hello = async (ctx, next) => {
-//     ctx.response.type = 'text/html';
-//     let name = ctx.request.query.name || 'world';
-//     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-// };
-
-// module.exports = {
-//     'GET /hello/:name': fn_hello
-// };
-
-// Nunjucks 模板方法
-// let fn_index = async (ctx, next) => {
-//     ctx.render('index.html', {N
-//         title: 'Welcome'
-//     });
-// };
-
-// module.exports = {
-//     'GET /': fn_index
-// };
-
-module.exports = {
-    'GET /': async (ctx, next) => {
-        // websocket demo
-        // let user = ctx.state.user;
-        // if (user) {
-        //     ctx.render('roomVue.html', {
-        //         user: user
-        //     });
-        // } else {
-        //     ctx.response.redirect('/signin');
-        // }
-
-        // mysql demo
-        ctx.render('index.html');
-
-        // rest demo
-        // ctx.render('indexVue.html');
-    }
-};
+module.exports = async ( ctx ) => {
+    const title = 'home'
+    await ctx.render('index', {
+      title
+    })
+  }
