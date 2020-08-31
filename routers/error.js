bb284200ae1097c0c6051d5d621746d870ea5850
xuyangyang2019 <15268296911@163.com>
2020-08-31 18:23:30 +0800

    代码优化

--- routers/error.js
+++ routers/error.js
@@ -4,9 +4,9 @@
 
 const router = require('koa-router')()
 
-module.exports = router.get('*',  async ( ctx ) => {
+module.exports = router.get('*', async (ctx) => {
   const title = 'error'
-  await ctx.render('error', {
+  await ctx.render('error.html', {
     title
   })
 })
