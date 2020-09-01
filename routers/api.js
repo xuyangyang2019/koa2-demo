9e5d9f999593f08d35b2f7b8e27283a4666b7b37
xuyangyang2019 <15268296911@163.com>
2020-09-01 10:50:52 +0800

    接口子路由

--- routers/api.js
+++ routers/api.js
@@ -4,12 +4,12 @@
  */
 
 const router = require('koa-router')()
-const userInfoController = require('./../controllers/user-info')
+const userInfoController = require('../controllers/user')
 
 const routers = router
-  .get('/user/getUserInfo.json', userInfoController.getLoginUserInfo)
-  .post('/user/signIn.json', userInfoController.signIn)
-  .post('/user/signUp.json', userInfoController.signUp)
+  .get('/user/getUserInfo', userInfoController.getLoginUserInfo)
+  .post('/user/signIn', userInfoController.signIn)
+  .post('/user/signUp', userInfoController.signUp)
  
   
 module.exports = routers
