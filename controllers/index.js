9f3ffd6ad2cc3325e6f7a74efccafbf09295cbd7
xuyangyang2019 <15268296911@163.com>
2020-09-02 20:03:46 +0800

    删除注释的代码

--- controllers/index.js
+++ controllers/index.js
@@ -1,26 +1,3 @@
-// MVC Controllers
-
-// 原始语法
-// let fn_hello = async (ctx, next) => {
-//     ctx.response.type = 'text/html';
-//     let name = ctx.request.query.name || 'world';
-//     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-// };
-
-// module.exports = {
-//     'GET /hello/:name': fn_hello
-// };
-
-// Nunjucks 模板方法
-// let fn_index = async (ctx, next) => {
-//     ctx.render('index.html', {N
-//         title: 'Welcome'
-//     });
-// };
-
-// module.exports = {
-//     'GET /': fn_index
-// };
 
 module.exports = {
     'GET /': async (ctx, next) => {
