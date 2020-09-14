6e3d29bd416fc7485351ae3aaf46e543dd962313
xuyangyang2019 <15268296911@163.com>
2020-09-14 17:59:37 +0800

    删除用不到的文件和代码

--- controllers/index.js
+++ controllers/index.js
@@ -4,17 +4,5 @@ module.exports = {
         ctx.render('index.html', {
             title: 'koa2-unjucks-demo'
         })
-        // websocket demo
-        // let user = ctx.state.user;
-        // if (user) {
-        //     ctx.render('roomVue.html', {
-        //         user: user
-        //     });
-        // } else {
-        //     ctx.response.redirect('/signin');
-        // }
-
-        // rest demo
-        // ctx.render('indexVue.html');
     }
 };
